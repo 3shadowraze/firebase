05d3fae73dc28ac2ac923b91ae16369acd10d82c
AmiRo <amirkhayatpour@gmamil.com>
2022-05-11 17:18:17 +0430

    firebase setup

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->enum('type',['passenger','driver']);
+            $table->string('device_key')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/push-notificaiton', [NotificationController::class, 'home'])->name('push-notificaiton');
+Route::post('/store-token', [NotificationController::class, 'storeToken'])->name('store.token');
+Route::post('/send-web-notification', [NotificationController::class, 'sendWebNotification'])->name('send.web-notification');
+
 Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth'])->name('dashboard');
 Route::get('/taxi/call/{driver}', [NotificationController::class, 'notifyToDriver'])->middleware(['passenger']);
 
